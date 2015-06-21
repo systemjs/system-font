d7bd8edf006f2499db3182fc4b615abf34388a07
David Craven <david@craven.ch>
2015-06-21 13:27:28 +0200

    Removes stmt taking substring(1) of fontName substring(1) of "google" results in "oogle"

--- font.js
+++ font.js
@@ -17,7 +17,6 @@ exports.fetch = function(load) {
           inactive: function() { reject() }
         };
 
-    fontName = fontName.trim().substr(1);
     config[fontName] = font
 
     switch (fontName) {
