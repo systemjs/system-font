ca90b02eded2e27072742af6422d88f32224542a
Luke Lazurite <aoser97+dev@gmail.com>
2016-01-25 21:26:36 +0800

    Refactor plugin.
    Change import syntax.
    Change to ES6 Module.

--- font.js
+++ font.js
@@ -1,36 +1,57 @@
-var WebFont = require("webfont"),
-    stripDomain = new RegExp("^(.*://[^/]*/)?(.*)$");
-    //                     scheme^ domain^ font^
+import WebFont from 'webfont';
 
-exports.locate = function(load) {
-  return decodeURI(load.name.match(stripDomain)[2]);
+export function locate(load) {
+  //TODO: (Luke) Better Regular Expression to trim the address
+  var stripDomain = new RegExp('.*\/(.*)');
+  return decodeURI(load.address.match(stripDomain)[1]);
 }
 
-exports.fetch = function(load) {
+export function fetch(load) {
   return new Promise(function(resolve, reject) {
-    var fontName = load.address.split(" ")[0],
-        params = load.address.substr(fontName.length + 1).split(",").map(function(str) {
-          return str.trim();
-        }),
-        font = {},
-        config = {
-          active: function() { resolve("") },
-          inactive: function() { reject() }
-        };
-
-    config[fontName] = font
-
-    switch (fontName) {
-      case "fontdeck":
-      case "typekit":
-        font.id = params.join(",");
+    var config = {
+      active: function() {
+        resolve('');
+      },
+      inactive: function() {
+        reject('Font loading failure');
+      }
+    };
+    //TODO: (Luke) Better Regular Expression to match the address
+    var [ ,service, fontName, version, timeout] = load.address.match(/(?:(\w+)@)?([\w\ +-:]+)(?:@([^#]*))?(?:#(\d+))?/);
+    if (! service) {
+      service = 'google';
+    }
+    if (! fontName) {
+      reject('Font name cannot be undefined');
+    }
+    switch (service) {
+      case 'fontdeck':
+        config[service] = {
+          'id': fontName
+        }
         break;
-      case "google":
-        font.families = params;
+      case 'monotype':
+        config[service] = {
+          projectId: fontName
+        }
+        if (version) {
+
+        }
+      case 'google':
+        config[service] = {
+          'families': [fontName]
+        }
         break;
-      case "monotype":
-        font.projectId = params[0];
-        font.version = params[1];
+      case 'typekit':
+        config[service] = {
+          'id': fontName
+        }
+      default:
+        reject('Unknown font service');
+    }
+
+    if (timeout) {
+      config.timeout = timeout;
     }
 
     WebFont.load(config);
